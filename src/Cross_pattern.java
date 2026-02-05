101d844dd28c8cc23822000846153ca042b2ccca
Deviljaga <dasjaga5140@gmail.com>
2026-02-05 10:26:04 +0530

    jagannath Das

---
+++ src/Cross_pattern.java
@@ -0,0 +1,6 @@
+public class Cross_pattern {
+
+    public static void main(String[] args) {
+        System.out.println("jaga");
+    }
+}
--- src/Inverse_pyramid2.java
+++ src/Inverse_pyramid2.java
@@ -1,6 +1,6 @@
 public class Inverse_pyramid2 {
     public static void main(String[] args) {
-        int n = 10;
+        int n = 1000;
         for(int i = n; i>=1;i--) {
             for(int j = n; j>=1;j--){
                 if(i>=j){
@@ -16,3 +16,4 @@ public class Inverse_pyramid2 {
         }
     }
 }
+
