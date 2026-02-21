09545b176f2a5fba555a63e18ba10c4b4933fdd0
Deviljaga <dasjaga5140@gmail.com>
2026-02-21 11:56:51 +0530

    jagannath das

---
+++ src/Fibonacci.java
@@ -0,0 +1,15 @@
+class Solution6 {
+    public int fib(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        return fib(n-1)+fib(n+2);
+    }
+}
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Solution6 s6 = new Solution6();
+        int result = s6.fib(6);
+        System.out.println(result);
+    }
+}
